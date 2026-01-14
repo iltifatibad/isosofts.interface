c7c23a904d83d1b5673cb91c6c5c8daea2e432f8
iltifatibad <iltifatibad@gmail.com>
2026-01-14 20:37:39 +0000

    Stage 3

--- components/tabledatas/fbrisk.jsx
+++ components/tabledatas/fbrisk.jsx
@@ -669,9 +669,9 @@ const FbBody = ({
                     className="border border-gray-200 px-3 py-2 w-32"
                     rowSpan={1}
                   >
-                    {row.nameOfVendor && (
+                    {row.customerName && (
                       <span className="inline-block px-3 py-1 bg-green-100 text-green-700 border border-green-200 rounded-full shadow-sm">
-                        {row.scope?.value}
+                        {row.customerName}
                       </span>
                     )}
                   </td>
