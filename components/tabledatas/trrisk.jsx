c7fcde6594602078018f2bac4b24bd86df92ca0b
iltifatibad <iltifatibad@gmail.com>
2026-01-15 10:20:38 +0000

    Stage 5

--- components/tabledatas/trrisk.jsx
+++ components/tabledatas/trrisk.jsx
@@ -375,16 +375,15 @@ const TrBody = ({
                     {row.competencyStatus}
                   </td>
 
-                                    <td
-                    className="border border-gray-200 px-3 py-2 w-32"
-                    rowSpan={1}
-                  >
-                    {row.effectiveness && (
-                      <span className="inline-block px-3 py-1 bg-green-100 text-green-700 border border-green-200 rounded-full shadow-sm">
-                        {row.effectiveness}
-                      </span>
-                    )}
-                  </td>
+<td
+  className="border border-gray-200 px-3 py-2 w-32"
+  rowSpan={1}
+>
+  <span className="inline-block px-3 py-1 bg-green-100 text-green-700 border border-green-200 rounded-full shadow-sm">
+    {row.effectiveness === 1 ? "Yes" : "No"}
+  </span>
+</td>
+
                 </tr>
 
                 {/* Ek Actions */}
@@ -542,16 +541,15 @@ const TrBody = ({
   })()}
 </td>
 
-                  <td
-                    className="border border-gray-200 px-3 py-2 w-32"
-                    rowSpan={1}
-                  >
-                    {row.effectiveness && (
-                      <span className="inline-block px-3 py-1 bg-green-100 text-green-700 border border-green-200 rounded-full shadow-sm">
-                        {row.effectiveness}
-                      </span>
-                    )}
-                  </td>
+<td
+  className="border border-gray-200 px-3 py-2 w-32"
+  rowSpan={1}
+>
+  <span className="inline-block px-3 py-1 bg-green-100 text-green-700 border border-green-200 rounded-full shadow-sm">
+    {row.effectiveness === 1 ? "Yes" : "No"}
+  </span>
+</td>
+
                 </tr>
 
                 {/* Ek Actions */}
@@ -985,16 +983,15 @@ const TrBody = ({
   })()}
 </td>
 
-                  <td
-                    className="border border-gray-200 px-3 py-2 w-32"
-                    rowSpan={1}
-                  >
-                    {row.effectiveness && (
-                      <span className="inline-block px-3 py-1 bg-green-100 text-green-700 border border-green-200 rounded-full shadow-sm">
-                        {row.effectiveness}
-                      </span>
-                    )}
-                  </td>
+<td
+  className="border border-gray-200 px-3 py-2 w-32"
+  rowSpan={1}
+>
+  <span className="inline-block px-3 py-1 bg-green-100 text-green-700 border border-green-200 rounded-full shadow-sm">
+    {row.effectiveness === 1 ? "Yes" : "No"}
+  </span>
+</td>
+
                 </tr>
               </React.Fragment>
             );
