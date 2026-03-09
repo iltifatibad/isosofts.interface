dd7e448916fbebef4ec31f5970af5123d73f0dc6
iltifatibad <iltifatibad@gmail.com>
2026-03-09 10:50:23 +0000

    Auth Token

--- utils/isAuth.js
+++ utils/isAuth.js
@@ -1,11 +1,25 @@
-export const isAuth = () => {
-    let token = getCookie("auth_token") || null;
-    if (!!token == null) {
-        window.location.href = "http://isosofts.com/los";
-        console.log(" Token Can't Find")
-    } else {
-        console.log(" Token Find And ReDirecting ")
-        window.location.href = "http://algebra.isosofts.com";
-        return !!token
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift();
     }
-} 
+
+    return null;
+}
+
+export const isAuth = () => {
+
+    let token = getCookie("auth_token");
+
+    if (!token) {
+        console.log("Token Can't Find");
+        window.location.href = "http://isosofts.com/los";
+        return false;
+    }
+
+    console.log("Token Find And ReDirecting");
+    window.location.href = "http://algebra.isosofts.com";
+    return true;
+}
