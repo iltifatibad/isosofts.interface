6f6479d31e2a190c214bf68d7787a62621ca0589
iltifatibad <iltifatibad@gmail.com>
2026-03-09 21:44:58 +0000

    Edit Modal Bug Fix

--- components/admin.jsx
+++ components/admin.jsx
@@ -725,7 +725,7 @@ const openEditEmployee = (emp) => {
                               </td>
                               <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-3">
                                 <button
-                                  onClick={() => openEditEmployee(emp.id)}
+                                  onClick={() => openEditEmployee(emp)}
                                   className="text-blue-600 hover:text-blue-800"
                                 >
                                   Edit
