9f3e56982508b3fc6e82f9ce9987fee233fb61f7
iltifatibad <iltifatibad@gmail.com>
2026-03-10 00:10:08 +0000

    Account Some Bug Fix

--- components/userprofile.jsx
+++ components/userprofile.jsx
@@ -149,7 +149,7 @@ useEffect(() => {
 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 <div>
 <label className="block text-sm font-medium text-gray-500">Account</label>
-<p className="mt-1 text-gray-900 font-medium">{profile.isActive}</p>
+<p className="mt-1 text-gray-900 font-medium">{profile.isActive === 1 ? "Active" : "Deactive"}</p>
 </div>
 <div>
 <label className="block text-sm font-medium text-gray-500">Email</label>
